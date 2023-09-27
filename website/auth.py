3f6810f0b550e6b5ebf9e11865bbcce784c67f8f
JustPC <robert49324@gmail.com>
2023-09-27 21:35:48 +0300

    Create DB and write login logic

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, request, flash
+from .models import User
+from werkzeug.security import  generate_password_hash, check_password_hash
+
 
 auth = Blueprint('auth',__name__)
 
@@ -28,5 +31,11 @@ def sign_up():
         elif len(password1) < 7:
             flash("Password must be at least 7 characters", category='error')
         else:
-            flash("Accout created", category='success')
+            new_user = User(email=email,firstName=firstName,password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created', category='success')
+            
+            
+            
     return render_template("sign_up.html")
