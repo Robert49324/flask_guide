50ca36911dcbf381ab3aede24ef4c818a7a63ceb
JustPC <robert49324@gmail.com>
2023-09-27 20:57:25 +0300

    Added

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
  
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = 'thvertvhert'
-    
     app.secret_key = 'thvertvhert'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
